a43191c68ada575aeaebe1d5da40cf15b4efa7b2
Assafkatz99 <assafkatz99@gmail.com>
2023-05-11 18:44:09 +0300

    adding env file + creating connection to mongodb

--- apps/my-nest-app/src/app/app.module.ts
+++ apps/my-nest-app/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WorkoutsModule } from '../workouts/workouts.module';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import dotenv from "dotenv"
+dotenv.config();
+ 
 @Module({
-  imports: [MongooseModule.forRoot(''),WorkoutsModule],
+  imports: [MongooseModule.forRoot(`mongodb+srv://${process.env.MONGO_USER_NAME}:${process.env.MONGO_USER_PASSWORD}@nx-training-cluster.rrqp01a.mongodb.net/`),WorkoutsModule],
   controllers: [],
   providers: [],
 })
